7ec21c114852eb8bfec771fce95aaa5b06bbb89a
MusaOglanov <oglanovmusa2001@gmail.com>
2023-02-22 22:54:40 +0400

    Sql Connection

---
+++ AllUp3/AllUp3/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AllUp3.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+
+    }
+}
--- AllUp3/AllUp3/Program.cs
+++ AllUp3/AllUp3/Program.cs
@@ -1,8 +1,16 @@
+using AllUp3.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
